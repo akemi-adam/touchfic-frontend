ef43945ba77b8aae9cf6d20f12fe52b1890dbdb6
Akemi Adam <90357785+akemi-adam@users.noreply.github.com>
2023-02-20 16:31:23 -0300

    Corrigindo erro de sintax nas classes das tags do AuthNavbar

--- src/components/AuthNavbar.jsx
+++ src/components/AuthNavbar.jsx
@@ -5,11 +5,11 @@ export default function AuthNavbar()
     return (
         <>
             <li className="nav-item dropdown me-3">
-                <button type="button" class="btn btn-link text-decoration-none p-0" data-bs-toggle="dropdown" aria-expanded="false">Usuário</button>
-                <ul class="dropdown-menu dropdown-menu">
-                    <li><a class="dropdown-item" href="#">Perfil</a></li>
-                    <li><a class="dropdown-item" href="#">Minhas histórias</a></li>
-                    <li><a class="dropdown-item" href="#">Publicar postagem</a></li>
+                <button type="button" className="btn btn-link text-decoration-none p-0" data-bs-toggle="dropdown" aria-expanded="false">Usuário</button>
+                <ul className="dropdown-menu dropdown-menu">
+                    <li><a className="dropdown-item" href="#">Perfil</a></li>
+                    <li><a className="dropdown-item" href="#">Minhas histórias</a></li>
+                    <li><a className="dropdown-item" href="#">Publicar postagem</a></li>
                 </ul>
             </li>
             <li className="nav-item me-3"><a className="nav-link" href="#">Histórias</a></li>
